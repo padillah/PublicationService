7599a5bb0c07918f2436e370e46369b5b42599e4
Henry Padilla <padilla.henry@gmail.com>
2016-06-30 05:39:47 -0700

    Renaming and removing extra files.

--- EventInfo.cs
+++ EventInfo.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PublicationSubscription
+namespace PublicationSubscriptionService
 {
     public class EventInfo
     {
--- PublicationService.cs
+++ PublicationService.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
-using PublicationService;
 
-namespace PublicationSubscription
+namespace PublicationSubscriptionService
 {
     public class PublicationService:IPublicationService
     {
